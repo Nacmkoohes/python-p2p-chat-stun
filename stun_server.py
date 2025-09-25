0a22327e41929f73c24a8e868346a9c470667882
Nasim Koohestani <119655711+Nacmkoohes@users.noreply.github.com>
2025-09-25 15:42:13 +0330

    Update stun_server.py

--- stun_server.py
+++ stun_server.py
@@ -31,4 +31,5 @@ def peerinfo():
     return jsonify({'address': result.decode('utf-8')}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=8001, debug=False)
+
